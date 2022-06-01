fda21e79588ef84e16f15b54d4b5ec262b3a894a
SiantyX <siantyx@gmail.com>
2022-06-02 00:36:19 +0200

    test all casts

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { getConsumes } from "./parselogs.js"
+import fs from "fs"
+const consumeList = JSON.parse(fs.readFileSync("./big_pyll_consumables_list_in_ids.json"));
 
 /*
   const query = {
@@ -19,14 +21,25 @@ import { getConsumes } from "./parselogs.js"
   }
 */
 
-import fs from "fs"
-const ql = [
-  { alias: "manapots", dataType: "cast", abilityID: 28499, startTime: 0 },
-  { alias: "darkrunes", dataType: "cast", abilityID: 27869, startTime: 0 }
-];
 const logId = "gWkrGhyX7FJmvnVC";
-//getConsumes(ql, logId).then(r => console.log(r));
+const dataType = "cast"
+Object.keys(consumeList[dataType]).map(category => {
+  const ql = [];
+  /*
+  const ql = [
+    { alias: "manapots", dataType: "cast", abilityID: 28499, startTime: 0 },
+    { alias: "darkrunes", dataType: "cast", abilityID: 27869, startTime: 0 }
+  ];
+  */
+  
+  //getConsumes(ql, logId).then(r => console.log(r));
 
-getConsumes(ql, logId).then(r => {
-  fs.writeFileSync("./test.json", JSON.stringify(r, null, 2) , "utf-8");
+  consumeList[dataType][category].map(id => {
+    ql.push({ alias: `id${id.toString()}`, dataType: dataType, abilityID: id, startTime: 0 });
+  });
+  
+  getConsumes(ql, logId).then(r => {
+    fs.writeFileSync(`./test/${category}.json`, JSON.stringify(r, null, 2) , "utf-8");
+  });
 });
+
